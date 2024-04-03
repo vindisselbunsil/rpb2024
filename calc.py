3aeb70da30af07bd0d69704ba8b593a7b3d9d26a
vindisselbunsil <vindisselbunsil@snu.ac.kr>
2024-04-03 15:50:41 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,12 +1,15 @@
+#1
 print("Hello World")
 
+#2
 def main():
     print("Hello World")
 main()
-
+#3
+#main.py
 import myPythonFile   
 myPythonFile.func()
-
+#myPythonFile.py
 def func():
     print("Hello World")
 if __name__ == "__main__":
@@ -16,6 +19,10 @@ else:
     print("import")
     print(__name__)
 
+
+
+#calc.py
+#addition
 def main():
     print("Let's implement addition. Type two numbers for x and y.")
 
@@ -24,29 +31,43 @@ def main():
     
     print("%d + %d = %d" % (x, y, add(x, y)))    
     
-
-# TODO: add() 함수 정의
 def add(x,y):
     return x+y
 
 if __name__ == "__main__":
     main()
-
-def main():
-    print("Let's implement division. Type two numbers for x and y")
     
+#subtraction
+def main():
+    print("Let's implement subtraction. Type two numbers for x and y.")
+
     x = int(input("x > "))
     y = int(input("y > "))
     
-    if y!=0:
-        print("%d / %d = %0.3f" % (x, y, divide(x, y)))
-    else: divide(x,y)
+    print("%d - %d = %d" % (x, y, subtract(x, y)))    
     
-def add(a, b):
-    return a + b
+def subtract(x,y):
+    return x-y
+
+if __name__ == "__main__":
+    main()
+
+#multiplication
+def main():
+    print("Let's implement multiplication. Type two numbers for x and y.")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
     
+    print("%d * %d = %d" % (x, y, multi(x, y)))    
     
-# TODO: divide() 함수 정의
+def multi(x,y):
+    return x*y
+
+if __name__ == "__main__":
+    main()
+
+#divide
 def divide(x,y):
     if y==0:
         print("Error: cannot divide by zero.")
